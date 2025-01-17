094f104d9feca2ac7d1a8250d886e30ae2e3975f
Juan Vanegas <juan.vanegas@loka.com>
2025-01-17 16:31:18 -0500

    :sparkles: Abstract logic into neural network module

--- neurite/neural_network.c
+++ neurite/neural_network.c
@@ -6,3 +6,36 @@ int predict() {
     printf("Predicting...\n");
     return 0;
 }
+
+float sigmoid(float x) { return 1 / (1 + exp(-x)); }
+
+void initialize_layer(struct Layer *layer, int layer_size, int output_size) {
+    layer->neurons_size = layer_size;
+    layer->neurons = (float *)malloc(layer_size * sizeof(float));
+
+    layer->biases = (float *)malloc(output_size * sizeof(float));
+    layer->weights = (float **)malloc(output_size * sizeof(float *));
+    for (int i = 0; i < output_size; i++) {
+        layer->weights[i] = (float *)malloc(layer_size * sizeof(float));
+    }
+}
+
+void free_layer(struct Layer *layer, int output_size) {
+    free(layer->neurons);
+    free(layer->biases);
+    for (int i = 0; i < output_size; i++) {
+        free(layer->weights[i]);
+    }
+    free(layer->weights);
+}
+
+void forward_propagation_step(struct Layer *a1, struct Layer *a0) {
+    for (int i = 0; i < a1->neurons_size; i++) {
+        printf("%d: %f\n", i, a1->neurons[i]);
+        float sum = 0;
+        for (int j = 0; j < a0->neurons_size; j++) {
+            sum += a0->neurons[i] * a0->weights[i][j];
+        }
+        a1->neurons[i] = sigmoid(sum + a1->biases[i]);
+    }
+}
--- neurite/neural_network.h
+++ neurite/neural_network.h
@@ -1,8 +1,27 @@
 #ifndef __NEURAL_NETWORK_H
 #define __NEURAL_NETWORK_H
 
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+struct Layer {
+    int neurons_size;
+    float *neurons;
+    float *biases;
+    float **weights;
+};
 
 void train();
+
 int predict();
+
+float sigmoid(float x);
+
+void initialize_layer(struct Layer *layer, int layer_size, int output_size);
+
+void free_layer(struct Layer *layer, int output_size);
+
+// a1_i = s(W_ij * a0_j + b_i)
+void forward_propagation_step(struct Layer *a1, struct Layer *a0);
 #endif // __NEURAL_NETWORK_H
